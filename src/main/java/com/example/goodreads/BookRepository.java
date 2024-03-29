cb160e37046e333f5d13ee03da06e1abab14c78a
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-03-29 16:49:10 +0530

    Update BookRepository.java

--- src/main/java/com/example/goodreads/BookRepository.java
+++ src/main/java/com/example/goodreads/BookRepository.java
@@ -1,13 +1,18 @@
 package com.example.goodreads;
+import java.util.ArrayList; //updated
 
 public interface BookRepository {
     ArrayList<Book> getBooks();
 
+
     Book getBookById(int bookId);
+
 
     Book addBook(Book book);
 
+
     Book updateBook(int bookId, Book book);
+
 
     void deleteBook(int bookId);
 }
